deda3a59786c83ed400b23d4a229da02177cdbe3
Jaison A <jaison2005k@gmail.com>
2026-06-25 17:05:02 +0530

    random changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-// const API = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-// });
-
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: 'http://localhost:5000/api',
 });
 
+// const API = axios.create({
+//   baseURL: `https://campushub-api-sv12.onrender.com/api`,
+// });
+
 export default API;
+// https://campushub-api-sv12.onrender.com
