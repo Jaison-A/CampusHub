94708e472cb74906c23563fed295baa1afbca76c
Jaison A <jaison2005k@gmail.com>
2026-06-25 18:25:47 +0530

    correct api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${API_URL}/api`,
 });
 
-// const API = axios.create({
-//   baseURL: `https://campushub-api-sv12.onrender.com/api`,
-// });
-
 export default API;
-// https://campushub-api-sv12.onrender.com
